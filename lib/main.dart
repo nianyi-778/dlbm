15e6df926b24a3c95231f4ba256834e6852f97ef
nian yi <nianyi@dtstack.com>
2023-05-19 22:18:46 +0800

    chore：启动屏

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:dlbm/config/router.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -10,6 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return const MaterialApp(
       onGenerateRoute: RouteConfiguration.generateRoute,
     );
